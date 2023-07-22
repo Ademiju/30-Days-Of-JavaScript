b795b7999b1e11befa8d22036ebb743e6a3e4a77
Ademiju Taiwo <hardaymyju@gmail.com>
2023-07-22 09:29:13 +0100

    day 1 assignment

---
+++ Day-1/datatypes.js
@@ -0,0 +1,27 @@
+let aString ='string', aBoolean = true, noValue, nullable = null
+//checking datatype of declared variables
+console.log(typeof(aString))
+console.log(typeof(aBoolean))
+console.log(typeof(noValue))
+console.log(typeof(nullable))
+
+//declaring variable without assigning values
+let name
+let religion
+let nationality
+let gender
+
+//declaring variable with values assigned
+let occupation = 'Software Engineer'
+let isMarried = false
+const numberOfSiblings = 1;
+let height = 1.86
+
+//declaring and assigning variables in one line
+let firstName = 'Ademiju',latstName = 'Taiwo', maritalStatus = 'Single', country='Nigeria', age= 28
+
+//printing ages to the console
+let myAge = 25
+let yourAge = 30
+
+console.log("I am "+myAge+" years old\nYou are " +yourAge+" years old")
